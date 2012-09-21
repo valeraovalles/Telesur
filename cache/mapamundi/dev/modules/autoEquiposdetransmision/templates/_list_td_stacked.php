364aeeee146de9de21c9e85bc5fe02aeb141fc38
root <root@aplicaciones.telesurtv.net>
2012-09-21 09:56:38 -0430

    gitignore

--- cache/mapamundi/dev/modules/autoEquiposdetransmision/templates/_list_td_stacked.php
+++
@@ -1,3 +0,0 @@
-<td colspan="2">
-  <?php echo __('%%id_equipo_transmision%% - %%descripcion_equipo_transmision%%', array('%%id_equipo_transmision%%' => link_to($MmEquiposTransmision->getIdEquipoTransmision(), 'mm_equipos_transmision_edit', $MmEquiposTransmision), '%%descripcion_equipo_transmision%%' => $MmEquiposTransmision->getDescripcionEquipoTransmision()), 'messages') ?>
-</td>
--- cache/mapamundi/prod/modules/autoEquiposdetransmision/templates/_list_th_stacked.php
+++
@@ -1,1 +0,0 @@
-<?php include_partial('equiposdetransmision/list_th_tabular', array('sort' => $sort)) ?>
