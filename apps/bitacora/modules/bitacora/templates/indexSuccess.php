e34052cb8778ed3fecc94e204ef8e0301bac0b77
valeraovalles <valeraovalles@gmail.com>
2012-12-27 13:27:04 -0430

    bitacora

--- apps/bitacora/modules/bitacora/templates/indexSuccess.php
+++ apps/bitacora/modules/bitacora/templates/indexSuccess.php
@@ -7,9 +7,4 @@
             <td><a href="<?php echo url_for("bitacora/listado")?>"><img src="<?php echo image_path("bitacora/registro.jpg")?>" width="120px"><br>REGISTRAR EN BITÁCORA</a></td>
         </tr>
         
-        <tr align="center">
-            <td><a href="<?php echo url_for("categorias/index")?>"><img src="<?php echo image_path("bitacora/categoria.jpeg")?>" width="120px"><br>ADMINISTRAR CATEGORIAS</a></td>
-        </tr>
-
-
 </table>
