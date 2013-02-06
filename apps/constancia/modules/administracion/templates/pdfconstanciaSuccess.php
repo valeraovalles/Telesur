49a92ffe5ca12f5772fe50632632f04cbf632041
valeraovalles <valeraovalles@gmail.com>
2013-02-06 17:01:10 -0430

    join con tipo nomina

--- apps/constancia/modules/administracion/templates/pdfconstanciaSuccess.php
+++ apps/constancia/modules/administracion/templates/pdfconstanciaSuccess.php
@@ -144,6 +144,7 @@ if($db){
             nmm024.cto_codcto < 1000 and  nmm024.cto_codcto <> 20 and
             nmm002.trab_fictra = nmm024.trab_fictra and
             nmm002.cto_codcto = nmm024.cto_codcto 
+            nmm002.tnom_tipnom = nmm024.tnom_tipnom 
 
         ";
    } else if($existe==0){
